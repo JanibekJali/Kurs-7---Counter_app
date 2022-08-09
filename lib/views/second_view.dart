279f5739e8556d9e0e5f1979d81f468ac49ff6f1
User <user@Users-MacBook-Pro.local>
2022-08-09 18:50:52 +0600

    Counter app Fully refactored

---
+++ lib/views/second_view.dart
@@ -0,0 +1,23 @@
+import 'package:counter_app/app_constants/colors/app_colors.dart';
+import 'package:counter_app/app_constants/texts/app_texts.dart';
+import 'package:counter_app/widgets/custom_app_bar.dart';
+import 'package:counter_app/widgets/custom_container.dart';
+import 'package:flutter/material.dart';
+
+class SecondView extends StatelessWidget {
+  const SecondView({this.kelgenSan});
+  final String kelgenSan;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const CustomAppBar(title: AppTexts.homework2),
+        body: Center(
+          child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: CustomContainer(
+                number: int.parse(kelgenSan),
+                color: AppColors.mainColor,
+              )),
+        ));
+  }
+}
